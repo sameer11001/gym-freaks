05a0fe4070a0abb698ff698990527a838ea666ec
samir ahmad <sameeralhajeid.prog@gmail.com>
2024-07-07 11:48:01 +0300

    nothing just remove public from test

--- src/test/java/org/webapp/gymfreaks/auth/repository/AuthRepositoryTests.java
+++ src/test/java/org/webapp/gymfreaks/auth/repository/AuthRepositoryTests.java
@@ -15,20 +15,17 @@ import java.util.Set;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class AuthRepositoryTests {
+class AuthRepositoryTests {
     @Autowired
     UserRepository userRepository;
 
     @Autowired
     RoleRepository roleRepository;
 
-
-
     @Test
-    public void UserRepository_SaveUser_ReturnsSavedUser() {
+    void UserRepository_SaveUser_ReturnsSavedUser() {
 
-
-        //Arrange
+        // Arrange
         if (roleRepository.findAll().isEmpty()) {
             roleRepository.save(new Role(null, Authorities.ADMIN.toString()));
             roleRepository.save(new Role(null, Authorities.USER.toString()));
@@ -38,21 +35,20 @@ public class AuthRepositoryTests {
         Set<Role> roles = new HashSet<>();
         roles.add(roleRepository.findByRoleName(Authorities.USER.toString()));
 
-
         UserEntity account = new UserEntity();
         account.setUserEmail("test@freaks.com");
         account.setUserPassword("12345678");
         account.setUserFirstName("test");
         account.setUserLastName("_user");
-        account.setUserPhoneNumber("067556234" );
+        account.setUserPhoneNumber("067556234");
         account.setUserDescription("im a test user_");
         account.setUserGender(Gender.MALE.toString());
         account.setUserImage("");
         account.setUserRoles(roles);
 
-        //Act
+        // Act
         userRepository.save(account);
-        //Assert
+        // Assert
         Assertions.assertThat(account).isNotNull();
         Assertions.assertThat(account.getUserId()).isGreaterThan(0);
     }
